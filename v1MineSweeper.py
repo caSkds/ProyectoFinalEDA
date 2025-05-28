47a427688b4fd644aaf370c6941e8cc608fcce4e
Cesar <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-05-27 18:32:47 -0600

    Added radar function and separate frame for main game

--- v1MineSweeper.py
+++ v1MineSweeper.py
@@ -1,13 +1,30 @@
 import tkinter as tk
 import random
 import sys
+import time
 myApp = tk.Tk()
+
+startTime = time.time()
+gameFrame = tk.Frame(myApp)
+gameFrame.pack()
 
 #Número de bombas
 bombs = 10
+shields = 5
+radars = 3
+powerUps = 2
+
+
 
 map = []
+mapGenerated = False
 buttons = []
+specialButtons = {
+    "shield" : 100,
+    "radar" : 200,
+    "powerUp" : 300,
+    "bombs" : 500
+}
 
 # Altura del juego
 height  = 15
@@ -38,19 +55,51 @@ def printMap(gameMap: list):
     for i in range(0,len(gameMap)):
         print(gameMap[i])
 
+# Adding every game object:
+def addGameObject(gameMap: list, x_avoid :int, y_avoid : int, nProperty: int, propertyName :str):
+    # Making sure the first cell is right next to a bomb
+    startingX = x_avoid
+    startingY = y_avoid
+    if propertyName == "bombs":
+        if startingX == width - 1:
+            startingX = x_avoid -1
+        if startingY == height - 1:
+            startingY = y_avoid - 1
+        if x_avoid != width - 1 and y_avoid != height - 1:
+            gameMap[startingY+1][startingX+1] = specialButtons[propertyName]
+        else:
+            gameMap[startingY][startingX] = specialButtons[propertyName]
+        nProperty -= 1
+            
+    nProperty -= 1
+    while nProperty > 0:
+        for i in range(0,height):
+            for j in range(0,width):
+                if i!= y_avoid and j!=x_avoid and gameMap[i][j] == 0:
+                    if random.randint(0,100)<5 and nProperty > 0:
+                        gameMap[i][j] = specialButtons[propertyName]
+                        nProperty -= 1
 
-# Initialize the map with 0s and place bombs randomly
-while bombs >0:
+# Initialize the map with 0s and place all objects randomly
+def initializeMap(gameMap: list, x_0 : int, y_0:int, nBombs: int, nShields: int, nRadars:int, nPowerUps: int):
+    global mapGenerated
+    addGameObject(gameMap, x_0, y_0, nBombs, "bombs")
+    addGameObject(gameMap, x_0, y_0, nShields, "shield")
+    addGameObject(gameMap, x_0, y_0, nRadars, "radar")
+    addGameObject(gameMap, x_0, y_0, nPowerUps, "powerUp")
+    """
+    # Fills the map with number of surrounding bombs
     for i in range(0,height):
-        for j in range(0,width):
-            if random.randint(0,100)<5 and bombs > 0 and map[i][j] == 0:
-                map[i][j] = 500 # 500 is a bomb
-                bombs -= 1
+            for j in range(0,width):
+                if map[i][j] != 500:
+                    map[i][j] = getNeighborBombs(map,j,i)
+    """
+    mapGenerated = True
 
 
 
 
-
+# Gets the count of bombs surrounding a cell
 def getNeighborBombs(gameMap: list, x : int, y : int) -> int:
     count = 0
     for i in range(-1, 2):
@@ -60,40 +109,57 @@ def getNeighborBombs(gameMap: list, x : int, y : int) -> int:
                     count += 1
     return count
 
-for i in range(0,height):
-        for j in range(0,width):
-            if map[i][j] != 500:
-                map[i][j] = getNeighborBombs(map,j,i)
 
 
 
-#printMap(map)
+def radarEffect(gameMap: list, x: int, y: int, buttons: list): 
+    # This function will reveal all the cells in a 3x3 area around the radar
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if 0 <= x + i < width and 0 <= y + j < height:
+                if gameMap[y + j][x + i] == 0:
+                    buttons[y + j][x + i]["text"] = getNeighborBombs(gameMap, x + i, y + j)
+                else:
+                    buttons[y + j][x + i]["text"] = gameMap[y + j][x + i]
 
 
 
 
 
+# Defines behavior for left click on a button
 def buttonClick(gameMap: list, name : str,buttonSet : list):
+    global mapGenerated
+
     buttonHeight =int(name[:name.index('F')])
     buttonWidth = int(name[name.index('e')+1:])
+    if not mapGenerated:
+        initializeMap(gameMap, buttonWidth, buttonHeight, bombs, shields, radars, powerUps)
+        printMap(gameMap)
+
 
     if gameMap[buttonHeight][buttonWidth] == 500:
         buttonSet[buttonHeight][buttonWidth]["text"] = "BOMB!"
+    elif gameMap[buttonHeight][buttonWidth] == 100:
+        radarEffect(gameMap, buttonWidth, buttonHeight, buttonSet)
+    elif gameMap[buttonHeight][buttonWidth] == 200:
+        buttonSet[buttonHeight][buttonWidth]["text"] = "RADAR"
+    elif gameMap[buttonHeight][buttonWidth] == 300: 
+        buttonSet[buttonHeight][buttonWidth]["text"] = "POWER UP"
     else:
         buttonSet[buttonHeight][buttonWidth]["text"] =getNeighborBombs(gameMap,buttonWidth, buttonHeight)
 
-
+# Defines flagging a button
 def flag( event ):
-    event.widget.config(text="F", bg="yellow")
+    event.widget.config(text="F")
+
 
 
 
 # Create buttons for the grid
-
 for i in range(0,height):
     for j in range(0,width):
         currentName = str(i)+str(False)+str(j)
-        button  =tk.Button(myApp,
+        button  =tk.Button(gameFrame,
                 name = str(i)  +str(j),
                 command =  lambda buttonName = currentName: buttonClick(map,buttonName,buttons))
         #print(str(i)+str(j))
@@ -101,6 +167,7 @@ for i in range(0,height):
             row = i,
             column = j
         )
+        #checks os for right click
         if sys.platform == "darwin":
             button.bind("<Button-2>", flag)
         else:
