44ab10c1d035463133f799c40396b1d005eed706
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-05-31 19:14:49 -0600

    idk

--- v4MineSweeper.py
+++ v4MineSweeper.py
@@ -120,9 +120,9 @@ specialButtons = {
 }
 
 # Altura del juego
-height  = 15
+height  = 4
 # Ancho del juego
-width = 15
+width = 4
 
 totalCasillas = height * width
 casillasCubiertas = 0 # Contador de casillas abiertas
@@ -564,8 +564,8 @@ def buttonClick(gameMap: list, name : str,buttonSet : list):
         initializeMap(gameMap, buttonWidth, buttonHeight, bombs, shields, radars, powerUps)
         printMap(gameMap)
     
-    if gameMap[buttonHeight][buttonWidth] == -1:
-        return
+    #if gameMap[buttonHeight][buttonWidth] == -1:
+     #   return
 
     if buttonSet[buttonHeight][buttonWidth]["text"] == "🚩":
         remainingFlags += 1
@@ -575,8 +575,8 @@ def buttonClick(gameMap: list, name : str,buttonSet : list):
         if buttonSet[buttonHeight][buttonWidth]["text"] == "🚩":
             remainingFlags -= 1
         if remainingShields == 0:
-            global mina_actual
-            mina_actual = (buttonHeight, buttonWidth)
+            #global mina_actual
+            #mina_actual = (buttonHeight, buttonWidth)
             mostrarMinijuego()
         else:
             remainingShields-=1
@@ -615,6 +615,7 @@ if sys.platform == "darwin":
         gameFrame.columnconfigure(i, weight=1)
 
 # Create buttons for the grid
+
 for i in range(0,height):
     for j in range(0,width):
         currentName = str(i)+str(False)+str(j)
@@ -654,6 +655,7 @@ for i in range(0,height):
             button.bind("<Button-3>", flag)
         #print(type(map[i][j]))
         buttons[i][j] = button
+
 
 #Variable para el menu:
 menu_frame = tk.Frame(myApp,bg="black")
